e7b89f805eed029708cda43de35cf038dd14a3eb
Jiyoongrace <giyun3632@naver.com>
2024-06-11 16:53:02 +0900

    :sparkles: feat: Spring File Upload

--- fileupload/src/main/java/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/fileupload/service/FileUploadService.java
@@ -3,30 +3,61 @@ package fileupload.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.*;
+import java.util.Calendar;
 
 @Service
 public class FileUploadService {
     private static String SAVE_PATH = "/fileupload-files";
+    private static String URL_PATH = "/images";
+
     public String restore(MultipartFile file) {
         String url = null;
-        File uploadDirectory = new File(SAVE_PATH);
-        if(!uploadDirectory.exists()) {
-            uploadDirectory.mkdirs();
+
+        try {
+            File uploadDirectory = new File(SAVE_PATH);
+            if(!uploadDirectory.exists()) {
+                uploadDirectory.mkdirs();
+            }
+
+            if(file.isEmpty()) {
+                return url;
+            }
+
+            String originFilename = file.getOriginalFilename();
+            String extName = originFilename.substring(originFilename.lastIndexOf(".") + 1);
+            String saveFilename = generateSaveFilename(extName);
+            Long fileSize = file.getSize();
+
+            System.out.println("#######" + originFilename);
+            System.out.println("#######" + saveFilename);
+            System.out.println("#######" + fileSize);
+
+            byte[] data = file.getBytes();
+            OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
+            os.write(data);
+            os.close();
+
+            url = URL_PATH + "/" + saveFilename;
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
-
-        if(file.isEmpty()) {
-            return url;
-        }
-
-        String originFilename = file.getOriginalFilename();
-        Long fileSize = file.getSize();
-
-        System.out.println("#######" + originFilename);
-        System.out.println("#######" + fileSize);
-
-        file.getSize();
-
         return url;
+    }
+
+    private String generateSaveFilename(String extName) {
+        String filename = "";
+
+        Calendar calendar = Calendar.getInstance();
+        filename += calendar.get(Calendar.YEAR);
+        filename += calendar.get(Calendar.MONTH);
+        filename += calendar.get(Calendar.DATE);
+        filename += calendar.get(Calendar.HOUR);
+        filename += calendar.get(Calendar.MINUTE);
+        filename += calendar.get(Calendar.SECOND);
+        filename += calendar.get(Calendar.MILLISECOND);
+        filename += ("." + extName);
+
+        return filename;
     }
 }
